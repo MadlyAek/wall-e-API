afebb35696cd2ff6c4699484deab3fdd40bbf6d8
Khanoksin Suranart <khanoksin.sur@app.domain>
2018-06-18 16:52:44 +0700

    Hello World

---
+++ wall-e-API/wall-e-API/App_Code/config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wall_e_API.App_Code
+{
+    public class config
+    {
+        public static readonly string sysVersion = "first";
+    }
+}
---
+++ wall-e-API/wall-e-API/Control/_main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Nancy;
+
+namespace wall_e_API.Control
+{
+    public class _main : NancyModule
+    {
+        public _main() {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
